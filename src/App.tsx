630fe0c60c11c3985b2217a08f7b06ef26d53833
Juan Vicente <mstifler034@gmail.com>
2025-11-08 13:25:33 -0300

    mudanças no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// ISSO É COMPONENTE
+// função que utiliza o nome em PascalCase
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() { // um componente 
+    console.log("oi");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   // que retorna um jsx
+    return(<div>
+      <h1>Olá Mundo!</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>)
 }
 
-export default App
