95a7a8ca4e7ff7ed240002864dad4335a64a59df
Juan Vicente <mstifler034@gmail.com>
2025-11-08 13:48:42 -0300

    criando pastas de estilos global e de tema em css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 // ISSO É COMPONENTE
 // função que utiliza o nome em PascalCase
+import './styles/theme.css'
+import './styles/global.css'
 
 export function App() { // um componente 
     console.log("oi");
@@ -7,7 +9,7 @@ export function App() { // um componente
    // que retorna um jsx
     return(<div>
       <h1>Olá Mundo!</h1>
-      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing..</p>
     </div>)
 }
 
