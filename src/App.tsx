5cca9d14e3f2113d0405745a9bb00648fa99a4e6
Juan Vicente <mstifler034@gmail.com>
2025-11-09 23:02:21 -0300

    criando o heading e aperfeicoando o react

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 // ISSO É COMPONENTE
 // função que utiliza o nome em PascalCase
-import './styles/theme.css'
-import './styles/global.css'
+
+import { Heading } from './components/Heading';
+
+import './styles/global.css';
+import './styles/theme.css';
 
 export function App() { // um componente 
     console.log("oi");
 
    // que retorna um jsx
     return(<div>
-      <h1>Olá Mundo!</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing..</p>
+      <Heading/>
+      <p>JESUS SAVE!!!</p>
     </div>)
 }
 
